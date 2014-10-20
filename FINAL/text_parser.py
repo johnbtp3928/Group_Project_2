d0c0a87443b74a5eb5dff9d0386d14b8902fa161
Owen2284 <owen2284@googlemail.com>
2014-10-20 11:25:36 +0100

    Intial upload.

---
+++ FINAL/text_parser.py
@@ -0,0 +1,114 @@
+import string
+
+# List of "unimportant" words (feel free to add more)
+skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
+              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
+              'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
+              'i', 'if', 'in', 'into', 'is', 'it', 'its', 'large', 'later',
+              'like', 'little', 'main', 'me', 'mine', 'more', 'my', 'now',
+              'of', 'off', 'oh', 'on', 'please', 'small', 'some', 'soon',
+              'that', 'the', 'then', 'this', 'those', 'through', 'till', 'to',
+              'towards', 'until', 'us', 'want', 'we', 'what', 'when', 'why',
+              'wish', 'with', 'would']
+
+
+def filter_words(words, skip_words):
+    """This function takes a list of words and returns a copy of the list from
+    which all words provided in the list skip_words have been removed.
+    For example:
+
+    >>> filter_words(["help", "me", "please"], ["me", "please"])
+    ['help']
+
+    >>> filter_words(["go", "south"], skip_words)
+    ['go', 'south']
+
+    >>> filter_words(['how', 'about', 'i', 'go', 'through', 'that', 'little', 'passage', 'to', 'the', 'south'], skip_words)
+    ['go', 'passage', 'south']
+
+    """
+
+    for banned_word in skip_words:
+        if words.count(banned_word) > 0:
+            words.remove(banned_word)
+
+    return words
+
+    
+def remove_punct(text):
+    """This function is used to remove all punctuation
+    marks from a string. Spaces do not count as punctuation and should
+    not be removed. The funcion takes a string and returns a new string
+    which does not contain any puctuation. For example:
+
+    >>> remove_punct("Hello, World!")
+    'Hello World'
+    >>> remove_punct("-- ...Hey! -- Yes?!...")
+    ' Hey  Yes'
+    >>> remove_punct(",go!So.?uTh")
+    'goSouTh'
+    """
+    no_punct = ""
+    for char in text:
+        if not (char in string.punctuation):
+            no_punct = no_punct + char
+
+    return no_punct
+
+
+def normalise_input(user_input):
+    """This function removes all punctuation from the string and converts it to
+    lower case. It then splits the string into a list of words (also removing
+    any extra spaces between words) and further removes all "unimportant"
+    words from the list of words using the filter_words() function. The
+    resulting list of "important" words is returned. For example:
+
+    >>> normalise_input("  Go   south! ")
+    ['go', 'south']
+    >>> normalise_input("!!!  tAkE,.    LAmp!?! ")
+    ['take', 'lamp']
+    >>> normalise_input("HELP!!!!!!!")
+    ['help']
+    >>> normalise_input("Now, drop the sword please.")
+    ['drop', 'sword']
+    >>> normalise_input("Kill ~ tHe :-  gObLiN,. wiTH my SWORD!!!")
+    ['kill', 'goblin', 'sword']
+    >>> normalise_input("I would like to drop my laptop here.")
+    ['drop', 'laptop']
+    >>> normalise_input("I wish to take this large gem now!")
+    ['take', 'gem']
+    >>> normalise_input("How about I go through that little passage to the south...")
+    ['go', 'passage', 'south']
+
+    """
+    # Remove punctuation and convert to lower case
+    clean_string = remove_punct(user_input).lower().strip()
+
+    #Create local list for termporary storage.
+    input_list = []
+
+    # Split string into list elements. 
+    input_list = clean_string.split()
+
+    #if len(clean_string) > 0:
+    #    letterNum = 0
+    #    while letterNum < len(clean_string):
+    #        if clean_string[letterNum] == " " or letterNum == len(clean_string)-1:
+    #            if letterNum == len(clean_string)-1:
+    #                input_list.append(clean_string)
+    #                break
+    #            else:
+    #                input_list.append(clean_string[:letterNum].strip())
+    #                clean_string = clean_string[letterNum+1:].strip()
+    #                if len(clean_string) > 0:
+    #                    letterNum = 0
+    #                else:
+    #                    break
+    #        letterNum += 1
+
+    # Filter out unnessecary words.
+    return_list = filter_words(input_list, skip_words)
+
+    # Return list of words.
+    return return_list
+
---
+++ read_me.txt
@@ -0,0 +1,5 @@
+Feel free to create a folder here for any work you want to upload.
+
+From here on in we'll be working on the game in the FINAL folder.
+Feel free to edit it as needed, since we'll all be working on it.
+You can also copy and paste any code you have into it as well.
